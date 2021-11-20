724ec5cd6408001014c172faec08c64cc68ff9a1
returnZer0 <raofei23@gmail.com>
2021-11-20 23:57:39 +0800

    feat: loadPipeLine

--- src/DX12Render.cpp
+++ src/DX12Render.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "DX12Render.h"
+#include "dxgiformat.h"
 
 void DX12Render::OnInit()
 {
+	LoadPipeline();	
+	LoadAssets();
 }
 
 void DX12Render::OnUpdate()
@@ -14,5 +17,100 @@ void DX12Render::OnRender()
 }
 
 void DX12Render::OnDestroy()
+{
+}
+
+void DX12Render::LoadPipeline()
+{
+    UINT dxgiFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    // Enable the debug layer (requires the Graphics Tools "optional feature").
+    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+    {
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+
+            // Enable additional debug layers.
+            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+        }
+    }
+#endif
+
+    // Create D3DDevice
+    ComPtr<IDXGIFactory4> factory;
+    ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)));
+
+    if (m_useWarpDevice)
+    {
+        ComPtr<IDXGIAdapter> warpAdapter;
+        ThrowIfFailed(factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
+
+        ThrowIfFailed(D3D12CreateDevice(
+            warpAdapter.Get(),
+            D3D_FEATURE_LEVEL_11_0,
+            IID_PPV_ARGS(&mDevice)
+        ));
+    }
+    else
+    {
+        ComPtr<IDXGIAdapter1> hardwareAdapter;
+        GetHardwareAdapter(factory.Get(), &hardwareAdapter);
+
+        ThrowIfFailed(D3D12CreateDevice(
+            hardwareAdapter.Get(),
+            D3D_FEATURE_LEVEL_11_0,
+            IID_PPV_ARGS(&mDevice)
+        ));
+    }
+
+    // Create Command Queue
+    D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
+    commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+    ThrowIfFailed(mDevice->CreateCommandQueue(&commandQueueDesc,IID_PPV_ARGS(&mCommandQueue)));
+
+    // Create SwapChain
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.BufferCount = FrameCount;
+    swapChainDesc.Width = mWidth;
+    swapChainDesc.Height = mHeight;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SampleDesc.Count = 1;
+    
+    ComPtr<IDXGISwapChain1> swapChain;
+    ThrowIfFailed(factory->CreateSwapChainForHwnd(
+        mCommandQueue.Get(),
+        Win32Application::GetHwnd(),
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain
+    ));
+
+    // This sample does not support fullscreen transitions.
+    ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
+    
+    ThrowIfFailed(swapChain.As(&mSwapChain));
+    mFrameIndex = mSwapChain->GetCurrentBackBufferIndex();
+
+    // Create descriptor heaps
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+    }
+
+    
+    // Create Command List
+    
+
+
+
+}
+
+void DX12Render::LoadAssets()
 {
 }
--- src/DX12Render.h
+++ src/DX12Render.h
@@ -5,10 +5,30 @@ class DX12Render : public DXSample {
 public:
     DX12Render(UINT width, UINT height, std::wstring name):
         DXSample(width, height, name)
-    {};
+    {
+        mWidth = width;
+        mHeight = height;
+    };
 
     virtual void OnInit() override;
     virtual void OnUpdate() override;
     virtual void OnRender() override;
     virtual void OnDestroy() override;
+
+private:
+    void LoadPipeline();
+    void LoadAssets();
+
+private:
+    static const UINT FrameCount = 3;
+
+    UINT mWidth = 1;
+    UINT mHeight = 1;
+
+    UINT mFrameIndex = 0;
+
+    ComPtr<ID3D12Device>       mDevice;
+    ComPtr<ID3D12CommandQueue> mCommandQueue;
+    ComPtr<IDXGISwapChain3>    mSwapChain;
+    ComPtr<ID3D12CommandList>  mCommandList;
 };
